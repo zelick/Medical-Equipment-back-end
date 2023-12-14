ad4f2c24266e4425513b363506f9e639c19b59ce
anciii13 <anaa.radovanovic2001@gmail.com>
2023-12-14 23:23:43 +0100

    test: test functionalities

---
+++ isa-projekat/src/main/java/com/example/isaprojekat/repository/ItemRepository.java
@@ -0,0 +1,5 @@
+package com.example.isaprojekat.repository;
+
+public class ItemRepository {
+
+}
---
+++ isa-projekat/src/main/java/com/example/isaprojekat/service/ItemService.java
@@ -0,0 +1,9 @@
+package com.example.isaprojekat.service;
+
+import com.example.isaprojekat.repository.EquipmentRepository;
+import com.example.isaprojekat.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ItemService {
+    private ItemRepository itemRepository;
+}
