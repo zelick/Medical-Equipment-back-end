478b64d6dc51866499ef1ab5af3f3ac8f9ca6b17
anciii13 <anaa.radovanovic2001@gmail.com>
2024-01-23 22:10:53 +0100

    feat: complete display of all qr codes of the user's reservations

--- isa-projekat/src/main/java/com/example/isaprojekat/controller/QrCodeController.java
+++ isa-projekat/src/main/java/com/example/isaprojekat/controller/QrCodeController.java
@@ -4,6 +4,9 @@ import com.example.isaprojekat.dto.CompanyDTO;
 import com.example.isaprojekat.service.EmailService;
 import com.example.isaprojekat.service.QrCodeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,4 +30,17 @@ public class QrCodeController {
             return "Error while creating QR CODE";
         }
     }
+
+    @GetMapping("/getQRCodeData/{id}")
+    @CrossOrigin(origins = "http://localhost:4200")
+    public ResponseEntity<byte[]> getQRCodeImage(@PathVariable Integer id) {
+        try {
+            byte[] qrCodeBytes = qrCodeService.displayQRcode(id);
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_PNG).body(qrCodeBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
 }
