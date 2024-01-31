86ecd5726fb6d00ff5ce40b6044a516b1b63d716
zelick <kristinazelic0908@gmail.com>
2024-01-31 10:49:19 +0100

    Fixed Rabbit Config

--- isa-projekat/src/main/java/com/example/isaprojekat/config/RabbitConfig.java
+++ isa-projekat/src/main/java/com/example/isaprojekat/config/RabbitConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class RabbitMQConfig {
+public class RabbitConfig {
 
     @Bean
     public Queue orderQueue() {
